b73054092b685ca3d26d6db702cfc8b4156390b6
burnhambradley20 <116041544+burnhambradley20@users.noreply.github.com>
2023-05-08 12:52:21 -0500

    added graph

---
+++ Rainman/src/pages/GraphPage.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { IonPage, IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonIcon, } from '@ionic/react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { close } from 'ionicons/icons';
+import './GraphPage.css';
+
+interface GraphPageProps {
+  cardCount: Record<string, number>;
+  decks: number;
+  onClose: () => void;
+}
+
+const GraphPage: React.FC<GraphPageProps> = ({ cardCount, decks, onClose }) => {
+  const data = Object.entries(cardCount).map(([Card, count]) => ({
+    Card,
+    Remaining: decks * 4 - count,
+  }));
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Remaining Cards</IonTitle>
+          <IonButton slot="end" onClick={onClose}>
+            <IonIcon icon={close} />
+          </IonButton>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div className="chart-container">
+          <BarChart
+            width={window.innerWidth * 1}
+            height={window.innerHeight * 0.7}
+            data={data}
+            margin={{
+              top: 100,
+              right: 40,
+              left: 0,
+              bottom: 40,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Card" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Remaining" fill="#1e90ff" />
+          </BarChart>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default GraphPage;
